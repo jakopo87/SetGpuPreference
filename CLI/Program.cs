83e9d189d331310b5738a2d73ea002e0475ff6eb
Jacopo Galati <jacopo.galati@outlook.com>
2018-07-10 13:18:02 +0200

    initial commit

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,15 +1,78 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CLI
 {
+	class Constants
+	{
+		public const string Default = "GpuPreference=0;";
+		public const string PowerSaving = "GpuPreference=1;";
+		public const string MaxPerformance = "GpuPreference=2;";
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			foreach (var arg in args)
+			{
+				switch (arg)
+				{
+					case "-h":
+					case "--help":
+						PrintDetailedHelp();
+						break;
+					case "-l":
+					case "--list":
+						ListPreferences();
+						break;
+					default:
+						PrintUsage();
+						break;
+				}
+			}
+
+
 		}
+
+		private static void PrintUsage()
+		{
+			throw new NotImplementedException();
+		}
+
+		private static void PrintDetailedHelp()
+		{
+			throw new NotImplementedException();
+		}
+
+		private static void ListPreferences()
+		{
+			var key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\DirectX\UserGpuPreferences", true);
+			foreach (var item in key.GetValueNames())
+			{
+				Console.WriteLine(item + " = >" + GpuLabel(key.GetValue(item) as string));
+			}
+		}
+
+		private static string GpuLabel(string s)
+		{
+			switch (s)
+			{
+				case Constants.Default:
+					return "Default";
+				case Constants.PowerSaving:
+					return "Power saving";
+				case Constants.MaxPerformance:
+					return "Max performance";
+				default:
+					return "Unknown value";
+			}
+		}
+
 	}
 }
--- CLI/Properties/AssemblyInfo.cs
+++ CLI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // Le informazioni generali relative a un assembly sono controllate dal seguente 
 // set di attributi. Modificare i valori di questi attributi per modificare le informazioni
 // associate a un assembly.
-[assembly: AssemblyTitle("CLI")]
+[assembly: AssemblyTitle("SetGpuPreferences")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CLI")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Jacopo Galati")]
+[assembly: AssemblyProduct("SetGpuPreferences")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
