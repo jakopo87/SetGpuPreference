afec40e2f75b9b75f3538ed0636060cc33ae7565
Jacopo Galati <jacopo.galati@outlook.com>
2018-07-15 00:17:21 +0200

    remove unused using

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace CLI
 {
